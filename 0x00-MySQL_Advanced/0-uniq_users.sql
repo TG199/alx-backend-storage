5fffa5b8c223fcdebcac428a9634ebe8df454deb
Kelechi Ebiri <56020538+TG199@users.noreply.github.com>
2024-08-22 19:50:38 +0100

    Update 0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create a table named 'users' with the specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,        -- Integer, never null, auto-increment, primary key
+    email VARCHAR(255) NOT NULL UNIQUE,       -- Email, string (255 characters), never null, unique
+    name VARCHAR(255)                         -- Name, string (255 characters)
+);
