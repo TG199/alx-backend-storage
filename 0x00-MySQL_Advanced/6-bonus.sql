cae7c0f76c398617ab40de759fc2c084746dd27a
Kelechi Ebiri <56020538+TG199@users.noreply.github.com>
2024-08-22 21:51:03 +0100

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,24 +1,21 @@
 -- Create the AddBonus stored procedure
 DELIMITER //
 
-
 CREATE PROCEDURE AddBonus(
-    IN user_id INT,                     -- User ID
-    IN project_name VARCHAR(255),       -- Project name
-    IN score INT                        -- Score value
+    IN user_id INT,                 -- User ID
+    IN project_name VARCHAR(255),   -- Project name
+    IN score INT                    -- Score value
 )
 BEGIN
-    DECLARE project_id INT;             -- Variable to store the project ID
-
+    DECLARE project_id INT;         -- Variable to store the project ID
 
     -- Check if the project exists
     SELECT id INTO project_id
-    FROM peojects
+    FROM projects
     WHERE name = project_name;
 
-
-    -- If the projec tdoes not exist, create it
-    IF project_id is NULL THEN
+    -- If the project does not exist, create it
+    IF project_id IS NULL THEN
         INSERT INTO projects (name)
         VALUES (project_name);
 
@@ -26,8 +23,7 @@ BEGIN
         SET project_id = LAST_INSERT_ID();
     END IF;
 
-
-    -- Insert the corrections record
+    -- Insert the correction record
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, project_id, score);
 END;
