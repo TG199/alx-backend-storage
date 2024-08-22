97a43ad8db98fadcaf02e6b00fad9f5984f04647
Kelechi Ebiri <56020538+TG199@users.noreply.github.com>
2024-08-22 22:52:57 +0100

    Update 11-need_meeting.sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,12 @@
+-- Create the view 'need_meeting'
+CREATE VIEW need_meeting AS
+SELECT
+    name
+FROM
+    students
+WHERE
+    score < 80
+    AND (
+        last_meeting IS NULL
+        OR last_meeting < CURDATE() - INTERVAL 1 MONTH
+    );
