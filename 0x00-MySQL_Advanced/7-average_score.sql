1dc9a5ca0e5d976d0e8ca7dcc100e4da44b9ba3c
Kelechi Ebiri <56020538+TG199@users.noreply.github.com>
2024-08-22 22:27:45 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,24 +2,22 @@
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT                    -- User ID for which to compute the average score
+    IN p_user_id INT                   -- User ID for which to compute the average score
 )
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2); -- Variable to store the computed average score
+    DECLARE avg_score DECIMAL(10, 2);  -- Variable to store the computed average score
+
+    -- Initialize avg_score to 0 to handle cases where there are no scores
+    SET avg_score = 0;
 
     -- Compute the average score for the given user
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
-
-    -- If there are no corrections for the user, set the average score to NULL
-    IF avg_score IS NULL THEN
-        SET avg_score = 0;
-    END IF;
+    WHERE user_id = p_user_id;
 
     -- Insert or update the average score in the average_scores table
     INSERT INTO average_scores (user_id, average_score)
-    VALUES (user_id, avg_score)
+    VALUES (p_user_id, avg_score)
     ON DUPLICATE KEY UPDATE
         average_score = avg_score;
 END;
